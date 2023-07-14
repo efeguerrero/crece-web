976890fec7fd68c0a7f3f72eddb016e6b37b52e8
efeguerrero <juanfg.8640@gmail.com>
2023-07-14 18:15:03 -0300

    feature section done

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -28,7 +28,7 @@ export default function Example() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="text-gray-700 -m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -83,7 +83,7 @@ export default function Example() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-alpha px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="sm:ring-gray-900/10 fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-alpha px-6 py-6 sm:max-w-sm sm:ring-1">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -92,7 +92,7 @@ export default function Example() {
 
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="text-gray-700 -m-2.5 rounded-md p-2.5"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -100,13 +100,13 @@ export default function Example() {
             </button>
           </div>
           <div className="mt-16 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="divide-gray-500/10 -my-6 divide-y">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="font-regular -mx-3 block rounded-lg px-3 py-2 text-4xl font-bold text-white hover:bg-gray-50 hover:text-alpha"
+                    className="font-regular hover:bg-gray-50 -mx-3 block rounded-lg px-3 py-2 text-4xl font-bold text-white hover:text-alpha"
                   >
                     {item.name}
                   </a>
@@ -115,7 +115,7 @@ export default function Example() {
               <div className="py-6">
                 <a
                   href="#"
-                  className="font-regular -mx-3 block rounded-lg px-3 py-2.5 text-4xl font-bold text-bravo hover:bg-gray-50"
+                  className="font-regular hover:bg-gray-50 -mx-3 block rounded-lg px-3 py-2.5 text-4xl font-bold text-bravo"
                 >
                   Contact Us
                 </a>
