f2cdb6b15fe112cc251567ee3fd13027fb970b4c
efeguerrero <juanfg.8640@gmail.com>
2023-08-02 11:44:53 -0300

    fix:nav styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -121,7 +121,7 @@ export default function NavBar({ lang }) {
         <Dialog.Panel
           className={`${
             isSticky ? 'py-4' : 'py-6'
-          } sm:ring-gray-900/10 fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-alpha px-6  sm:ring-1`}
+          } sm:ring-gray-900/10 fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-alpha px-6 sm:ring-1`}
         >
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
