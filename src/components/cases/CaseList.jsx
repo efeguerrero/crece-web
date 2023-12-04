5b087b94ea8984be319c8535d3b0da5e6b6c7567
efeguerrero <juanfg.8640@gmail.com>
2023-12-04 15:32:31 -0300

    fixed response grid layout for blog cards

--- src/components/cases/CaseList.jsx
+++ src/components/cases/CaseList.jsx
@@ -3,7 +3,7 @@ import CaseCard from './CaseCard.jsx';
 
 const ArticleList = ({ cases }) => {
   return (
-    <div className="grid grid-cols-1 justify-items-center gap-8 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-[repeat(1,_minmax(0,_384px))] justify-items-stretch gap-8 sm:grid-cols-[repeat(2,_minmax(0,_384px))] lg:grid-cols-[repeat(3,_minmax(0,_384px))]">
       {cases.items.map((caseData, index) => (
         <CaseCard key={index} caseData={caseData} />
       ))}
