3e5fe24d320d6f317624410f32748e4afa2b3e58
efeguerrero <juanfg.8640@gmail.com>
2025-02-22 14:44:43 -0300

    added color button and changed layout

--- src/components/clients/components/logo-grid.tsx
+++ src/components/clients/components/logo-grid.tsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 
 interface LogoGridProps {
   clientsImgs: string[];
+  lang: string;
 }
 
-export default function LogoGrid({ clientsImgs }: LogoGridProps) {
+export default function LogoGrid({ clientsImgs, lang }: LogoGridProps) {
   const [showAll, setShowAll] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -23,37 +24,46 @@ export default function LogoGrid({ clientsImgs }: LogoGridProps) {
     isMobile && !showAll ? clientsImgs.slice(0, 10) : clientsImgs;
 
   return (
-    <div className="relative mx-auto max-w-7xl p-8">
-      <div className="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4">
-        {visibleLogos.map((img, index) => (
-          <div
-            key={index}
-            className={`flex items-center justify-center transition-all duration-300 ease-in-out ${
-              isMobile && index >= 10 && !showAll
-                ? 'translate-y-4 opacity-0'
-                : 'translate-y-0 opacity-100'
-            }`}
-          >
-            <img
-              className="max-h-[50px] max-w-[130px] object-contain"
-              src={img || '/placeholder.svg'}
-              alt={`Client Logo ${index + 1}`}
-              width="130"
-              height="50"
-            />
-          </div>
-        ))}
+    <section className="flex flex-col">
+      <div className="relative p-8">
+        <div className="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4">
+          {visibleLogos.map((img, index) => (
+            <div
+              key={index}
+              className={`flex items-center justify-center transition-all duration-300 ease-in-out ${
+                isMobile && index >= 10 && !showAll
+                  ? 'translate-y-4 opacity-0'
+                  : 'translate-y-0 opacity-100'
+              }`}
+            >
+              <img
+                className="max-h-[50px] max-w-[130px] object-contain"
+                src={img || '/placeholder.svg'}
+                alt={`Client Logo ${index + 1}`}
+                width="130"
+                height="50"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="absolute right-0 top-0 h-28 w-28 border-r-2 border-t-2 border-bravo"></div>
+        <div className="absolute bottom-0 left-0 h-28 w-28 border-b-2 border-l-2 border-bravo"></div>
       </div>
       {isMobile && clientsImgs.length > 10 && (
         <button
-          className="bg-gray-200 hover:bg-gray-300 mx-auto mt-6 block rounded px-4 py-2 transition-colors duration-200"
+          className="mx-auto mt-16 w-max rounded-md border-[1px] bg-bravo px-6 py-3 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 lg:hover:border-bravo lg:hover:bg-transparent lg:hover:text-bravo"
           onClick={() => setShowAll(!showAll)}
         >
-          {showAll ? 'Show Less' : 'Show More'}
+          {showAll
+            ? lang === 'en'
+              ? 'Show Less'
+              : 'Ver Menos'
+            : lang === 'en'
+            ? 'Show More'
+            : 'Ver Más'}
         </button>
       )}
-      <div className="absolute right-0 top-0 h-28 w-28 border-r-2 border-t-2 border-bravo"></div>
-      <div className="absolute bottom-0 left-0 h-28 w-28 border-b-2 border-l-2 border-bravo"></div>
-    </div>
+    </section>
   );
 }
